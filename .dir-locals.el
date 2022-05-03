3ef4a6404bdf0b747be86a7ede3ad023d9ca457b
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2022-05-03 17:45:47 +0100

    emacs: .dir-locals.el config to set RDD aliases
    Include aliases from practicalli/clojure-deps-edn on REPL startup for a REPL
    Driven Development workflow
    
    - `:env/dev` adds `dev` directory to classpath, used to include a user namespace
    in dev/user.clj file
    
    - `:env/dev` adds `test` directory to classpath for in-REPL unit testing
    
    - `:lib/reloaded` libraries to hotload libraries in a running repl and
    refresh (remove vars) from current namespace
    
    - `:inspect/portal` browser bases data navigation of results from function
    evaluation

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((clojure-mode . ((cider-repl-display-help-banner . nil)
+                  (cider-preferred-build-tool     . clojure-cli)
+                  (cider-clojure-cli-aliases      . ":env/dev:env/test:lib/reloaded:inspect/portal"))))
