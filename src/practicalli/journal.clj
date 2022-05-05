adbd27cb889d53c44832909cc8b2975a8db7dd38
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2022-05-05 14:19:40 +0100

    src: design journal namespace

---
+++ src/practicalli/journal.clj
@@ -0,0 +1,16 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Design Journal
+;;
+;; Capturing design experiments and decisions
+;; whilst developing the project
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+(ns practicalli.journal)
+
+
+#_{:clj-kondo/ignore [:redefined-var]}
+(comment
+
+  #_())
